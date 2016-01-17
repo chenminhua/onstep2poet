802a82290ff1117df8a615de114e7232190422e1
bigtom <chenmh@shanghaitech.edu.cn>
2016-01-17 15:38:54 +0800

    Initial Commit

---
+++ testjson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testjson
+//
+//  Created by 陈敏华 on 1/17/16.
+//  Copyright © 2016 陈敏华. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
